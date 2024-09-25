0a7d0ac7e4a774cd25dff99406aa3c802cd5b792
Moki <j.kicko64@gmail.com>
2024-09-25 16:16:12 +0200

    commit one school

---
+++ ulohy1.py
@@ -0,0 +1,15 @@
+#domáce ulohy
+
+#prva uloha z du
+#def fun1():
+#    for i in range(1, 11):
+#        print(i)
+
+#fun1()
+
+#druha uloha z du
+#def fun2(a):
+#    for i in range(1, a+1):
+#        print(i)
+
+#fun2(2)
