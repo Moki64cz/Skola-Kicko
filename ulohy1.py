804ee0a5eacb6c5f8892ee9161426cf2820f1891
Moki <j.kicko64@gmail.com>
2024-09-28 14:53:35 +0200

    druhy commit, domace ulohy hotove, 28/9

--- ulohy1.py
+++ ulohy1.py
@@ -1,15 +1,127 @@
-#domáce ulohy
+import math
 
-#prva uloha z du
-#def fun1():
-#    for i in range(1, 11):
-#        print(i)
+"""
+domáce ulohy
 
-#fun1()
+#prva uloha
+def uloha1():
+    for i in range(1, 11):
+        print(i)
 
-#druha uloha z du
-#def fun2(a):
-#    for i in range(1, a+1):
-#        print(i)
+uloha1()
 
-#fun2(2)
+#druha uloha
+def uloha2(a):
+    for i in range(1, a+1):
+        print(i)
+
+uloha2(2)dd
+
+#tretia uloha
+def uloha3(N):
+    for i in range(5, N+1, 2):
+       print(i)
+
+uloha3(15)
+
+#štrvtá uloha
+def uloha4(N):
+    pwr = 1
+    for i in range(1,N+1):
+        power = i**2
+        print(i, power),
+
+uloha4(9)
+
+#piata uloha
+def uloha5(x, y):
+    for i in range(x , y+1):
+        sqrt = i**(1/2)
+        rounded = round(sqrt, 2)
+        print(rounded)
+
+uloha5(1, 6)
+
+#šiesta uloha
+def uloha6():
+    for x in range(1, 20+1):
+        if x == 3:
+            print("error div by zero")
+        else:
+            y = (x**2-1) / (x-3)
+            print(f"x = {x}, y = {y}")
+
+uloha6()
+
+#siedma úloha
+def uloha7(N):
+    for i in range(1,N+1):
+        if i % 3 == 0:
+            print(i)
+
+uloha7(21)
+
+#ôsma úloha
+def uloha8(N):
+    for i in range(1,N+1):
+        if i % 2 == 0:
+            print(i)
+            
+uloha8(26)
+
+#deviata úloha
+def uloha9(z, k):
+    for i in range(z, k+1):
+        if i % 2 != 0:
+            print(i)
+
+uloha9(6, 10)
+
+#desiata úloha
+def uloha10(N):
+    for i in range(N, 0, -1):
+        print(i)
+
+uloha10(10)
+
+
+#jedenásta úloha
+def uloha11(N):
+    for i in range(0, N+1):
+        if i % 7 == 0 and i % 4 == 0:
+            print(i)
+
+uloha11(400)
+
+
+#dvanásta úloha
+def uloha12(N):
+    n = 0
+    for i in range(1, N+1):
+        n + n + i
+    print(n)
+
+uloha12(5)
+
+#trinásta úloha
+def uloha13(N):
+    n = 0
+    for i in range(1, N+1):
+        if i % 2 == 0:
+            n = n + i
+    print(n)
+
+uloha13(6)
+
+
+#štrnásta úloha
+def uloha14(x, y):
+    n = 0
+    for i in range(x, y+1):
+        if i % 8 == 0:
+            n = n + 1
+    print(n)
+
+uloha14(0, 400)
+
+"""
